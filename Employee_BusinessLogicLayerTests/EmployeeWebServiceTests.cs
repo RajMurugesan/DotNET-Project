b0c5f8b484e9efc78ab24d175184440c9d6cd3b9
RajMurugesan <murugesan.raj@yahoo.com>
2017-03-02 19:25:39 -0600

    DotNET
    DotNET

--- Employee_BusinessLogicLayerTests/EmployeeWebServiceTests.cs
+++ Employee_BusinessLogicLayerTests/EmployeeWebServiceTests.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EmployeeWebService;
-using Employee_DatabaseLayer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace EmployeeWebService.Tests
 {
@@ -13,9 +12,7 @@ namespace EmployeeWebService.Tests
     {
         [TestMethod()]
         public void GetEmployeeTest()
-        { 
-            var emp = new EmpDatabase();
-            var actual = emp.GetEmployee()[0].FirstName;
+        {
             Assert.Fail();
         }
     }
